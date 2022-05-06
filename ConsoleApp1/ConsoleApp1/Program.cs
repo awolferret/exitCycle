fc5e77740e6138f94c4ae2a506d54c51d28cf405
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-06 21:31:49 +0300

    3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,13 +7,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string exitCommand;
+            string exit = "exit";
 
             do
             {
                 Console.WriteLine("Чтобы закончить введите exit");
                 exitCommand = Console.ReadLine();
             }
-            while (exitCommand != "exit");
+            while (exitCommand != exit);
             Console.WriteLine("Выход из цикла");
         }
     }
